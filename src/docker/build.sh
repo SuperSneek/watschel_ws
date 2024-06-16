c33155bdc29bde31af279d41af1c6da9a3df81b4
Niklas Conen <niklas.conen@student.kit.edu>
2024-06-16 12:02:50 +0200

    robot description and rviz config

--- src/docker/build.sh
+++ src/docker/build.sh
@@ -2,10 +2,6 @@
 
 # needs docker buildx and the path to a ssh key that is authorized for github
 
-# clone our own code and move to container. otherwise we need to either create an
-# access token or copy ssh keys to the container
-git clone git@github.com:Nordegraf/HumanCenteredRobotics.git
-
 docker build --rm -t solo8 .
 
 xhost +
