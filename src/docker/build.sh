ebbf19034450ffc06acef7e2a00e162b22eaf808
Jonas Lewandrowski <joecarverde@gmail.com>
2024-07-11 11:45:20 +0200

    watschel changes

--- src/docker/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# needs docker buildx and the path to a ssh key that is authorized for github
-
-docker build --rm -t solo8 .
-
-xhost +
-docker run -i --net=host -t solo8 /bin/bash
--- src/docker/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /opt/ros/iron/setup.bash
-exec "$@"
