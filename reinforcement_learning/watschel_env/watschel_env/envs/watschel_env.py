ffc6fdfe5b3926d29d779bdba7eb8e7cd58d0024
Jonas Lewandrowski <joecarverde@gmail.com>
2024-08-28 12:00:36 +0200

    Progress on RL environment

--- reinforcement_learning/watschel_env/watschel_env/envs/watschel_env.py
+++ reinforcement_learning/watschel_env/watschel_env/envs/watschel_env.py
@@ -4,13 +4,30 @@ from gym import spaces
 import numpy as np
 from gym import ActionWrapper, ObservationWrapper, RewardWrapper, Wrapper
 from gym.spaces import Box, Discrete
+import time
+
+client = RemoteAPIClient()
+sim = client.require('sim')
+
+sim.setStepping(True)
+
+sim.startSimulation()
+while (t := sim.getSimulationTime()) < 3:
+    print(f'Simulation time: {t:.2f} [s]')
+    sim.step()
+sim.stopSimulation()
 
 class WatschelWorld(gym.Env):
 
 
     def __init__(self, render_mode=None, size=5):
-        self.size = size  # The size of the square grid
-        self.window_size = 512  # The size of the PyGame window
+        #Init coppeliasim api
+        self._reset_client()
+
+        self.sim.setStepping(True)
+
+
+        self.episode_index = 0
 
         # Observations are dictionaries with the agent's and the target's location.
         # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
@@ -32,31 +49,63 @@ class WatschelWorld(gym.Env):
         return None # todo: get observation from coppeliasim
     
     def _get_info(self):
-        return {"distance": np.linalg.norm(self._agent_location - self._target_location, ord=1)}
+        return {}
     
 
-    def reset():
-        pass
+    def reset(self, seed=None, options=None):
+        self.episode_index = self.episode_index + 1
+
+
+        # We need the following line to seed self.np_random
+        super().reset(seed=seed)
+
+        #Stop and restart simulation
+        self.sim.stopSimulation()
+        time.sleep(0.5)
+
+
+        #Reset client every 50 episodes
+        if self.episode_index % 50 == 0:
+            self._reset_client()
+            time.sleep(1)
+
+        
+        self.sim.startSimulation()
+
+
+
+    def _reset_client(self):
+        #Init coppeliasim api
+        client = RemoteAPIClient()
+        self.sim = client.require('sim')
+        self.bridge_handle = self.sim.getObject("/base_link_visual/zmq_bridge")
 
     def step(self, action):
         ## Send forces from action to coppeliasim
-        pass
+        self.sim.callScriptFunction("set_forces", self.bridge_handle, action)
 
 
         # An episode is done if the agent has reached the target
-        terminated = False ##ask coppeliasim if terminated
+        terminated = sim.getSimulationTime() < 5 and self.sim.callScriptFunction("is_fallen", self.bridge_handle)
         reward = self._get_reward()
         observation = self._get_obs()
         info = self._get_info()
+
+        if self.episode_index % 10 == 0:
+            print("Taking action: " + str(action))
+            print("Reward is " + str(reward))
+
+        for i in len(range(5)): #only do rewards every x steps
+            self.sim.step()
 
         return observation, reward, terminated, False, info
     
     def _get_reward(self):
         #get reward from coppeliasim
-        return 0
+        return self.sim.callScriptFunction("get_reward", self.bridge_handle)
     
     def close(self):
-        pass
+        self.sim.stopSimulation()
 
 class RelativePosition(ObservationWrapper):
     def __init__(self, env):
